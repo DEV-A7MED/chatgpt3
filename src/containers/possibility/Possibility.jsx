ec0aef214979922cc9ec97d5e5c10dd35d926abe
DEV-A7MED <elahwal45@gmail.com>
2023-01-09 01:44:47 +0200

    responsive edit

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -5,7 +5,7 @@ import './possibility.css';
 const Possibility = () => (
   <div className="gpt3__possibility section__padding" id="possibility">
     <div className="gpt3__possibility-image">
-      <img src={possibilityImage} alt="possibility" />
+      <img className='possibility-image' src={possibilityImage} alt="possibility" />
     </div>
     <div className="gpt3__possibility-content">
       <h4>Request Early Access to Get Started</h4>
